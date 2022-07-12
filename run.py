8bae9c8a58287eeaae20a482c685b8be5a1706ca
Pat McDonald <patmcdonald@me.com>
2022-07-12 11:44:22 +0000

    create index.html, about.html, add render_template

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
-@app.@app.route("/")
-def index():
-    return "Hello World"
 
-if  __name__ == "__main__":
-  app.run(
-    host = os.environ.get("IP", "0.0.0.0"),
-    port =  int(os.environ.get("PORT", "5000")),
-    debug = True)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
